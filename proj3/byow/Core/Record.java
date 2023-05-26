cb95e0ea2dc1b7794559210c816b5b1db3f1aedb
Shoomoon <informationz@163.com>
2023-05-25 17:17:36 -0700

    Create a new class "Record", which simplify save game and load game.

---
+++ proj3/byow/Core/Record.java
@@ -0,0 +1,71 @@
+package byow.Core;
+
+import byow.TileEngine.TETile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
+
+public class Record implements Serializable {
+    private TETile[][] world;
+    private Random random;
+    private Position avatar;
+    private Position door;
+    private List<Position> treasures;
+    private List<Position> guardians;
+    private List<Room> roomsList;
+    private int lives;
+    // Turn lights on or off, toggled with key 'H'
+    private boolean avatarSightOnly;
+    public Record(TETile[][] world, Random random, Position avatar, Position door,
+                  List<Position> treasures, List<Position> guardians, List<Room> roomsList,
+                  int lives, boolean avatarSightOnly) {
+        this.world = world;
+        this.random = random;
+        this.avatar = avatar;
+        this.door = door;
+        this.treasures = treasures;
+        this.guardians = guardians;
+        this.roomsList = roomsList;
+        this.lives = lives;
+        this.avatarSightOnly = avatarSightOnly;
+    }
+
+    public TETile[][] getWorld() {
+        return world;
+    }
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public Position getAvatar() {
+        return avatar;
+    }
+
+    public Position getDoor() {
+        return door;
+    }
+
+    public List<Position> getTreasures() {
+        return treasures;
+    }
+
+    public List<Position> getGuardians() {
+        return guardians;
+    }
+
+    public List<Room> getRoomsList() {
+        return roomsList;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public boolean getAvatarSightOnly() {
+        return avatarSightOnly;
+    }
+}
